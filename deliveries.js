fbde8062ccae540836e58969cca8c97458275f43
Aphrodice Imanizabayo <34691089+Aphrodis@users.noreply.github.com>
2018-11-15 16:35:47 +0200

    Update deliveries.js

--- deliveries.js
+++ deliveries.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressValidator = require('express-validator');
 const bodyParser = require('body-parser'); 
 const app = express();
 app.use(bodyParser.json());
@@ -9,10 +10,10 @@ app.get('/',(req,res)=>{
 });
 const parcels = [
 	{id: 1, owner: 'Aphrodice', pickup:'Rwanda', destination: 'Ghana', weight: 12, price: 14400},
-	{id: 2, owner: 'Ariel', pickup:'Nigeria', destination: 'DRC', weight: 7, price: 12800},
-	{id: 3, owner: 'Emile', pickup:'Tanzania', destination: 'Eritrea', weight: 23, price: 19700},
-	{id: 4, owner: 'Honorine', pickup:'South Africa', destination: 'Zambia', weight: 18, price: 16200},
-	{id: 5, owner: 'Jesca', pickup:'Egypt', destination: 'Rwanda', weight: 32, price: 21000}
+	{id: 2, owner: 'Ariel', pickup:'Nigeria', destination: 'DRC', weight: 7, price: 8400},
+	{id: 3, owner: 'Emile', pickup:'Tanzania', destination: 'Eritrea', weight: 23, price: 27600},
+	{id: 4, owner: 'Honorine', pickup:'South Africa', destination: 'Zambia', weight: 18, price: 21600},
+	{id: 5, owner: 'Jesca', pickup:'Egypt', destination: 'Rwanda', weight: 32, price: 38400}
 ];
 
 
@@ -32,8 +33,8 @@ app.post('/api/v1/parcels', (req, res) => {
 		weight,
 		price
 	};
-	if (isNaN(weight) || isNaN(price)){
-		res.send("The weight should be integer so that you can be able to get the total price for your parcels");
+	if (isNaN(weight)){
+		res.send({message:"The weight should be integer so that you can be able to get the total price for your parcels"});
 	} else {
 		parcels.push(parcelOrders);
 		res.send(parcelOrders);
@@ -58,7 +59,7 @@ app.get('/api/v1/parcels/:id', (req, res)=>{
 
 //Getting all parcel delivery orders by a specific user
 
-app.get('/api/v1/users/:owner', (req, res)=>{
+app.get('/api/v1/users/:owner/parcels', (req, res)=>{
 	const person = parcels.find((c) => c.owner === req.params.owner);
 	if(!person){
 		return res.status(404).send('The person with that name was not found');
@@ -72,7 +73,7 @@ app.get('/api/v1/users/:owner', (req, res)=>{
 
 app.put('/api/v1/parcels/:id/cancel', (req, res)=>{
 	const updatedOrders = parcels.find(c=> c.id === parseInt(req.params.id));
-	if(!updatedOrders) return res.status(404).send('The parcel with that Id was not found');
+	if(!updatedOrders) return res.status(404).send({message:'The parcel with that Id was not found'});
 	
 	parcels.splice(parcels.indexOf(updatedOrders),1); 
 	res.send(updatedOrders);
@@ -80,3 +81,4 @@ app.put('/api/v1/parcels/:id/cancel', (req, res)=>{
 
 const port = process.env.PORT || 3000;
 	app.listen(port, () => console.log(`Listening to port ${port}`));
+	module.exports=app;
