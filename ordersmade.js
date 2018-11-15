e242fbe18ad0bd433e89764aca2a57d8af494dec
Aphrodis <izabayoaphrodis@gmail.com>
2018-11-15 12:01:05 +0200

    Added changes to the UI

--- ordersmade.js
+++
@@ -1,6 +0,0 @@
-const Status=["Delivered", "In transit", "Not Delivered"];
-function (Status) {
-	if (Status==="Delivered" || Status==="In transit") {
-		console.log(You can not cancel the delivery order)
-	};
-}
